500eb81addd72246c1756e0b6f083997187387e2
Bruno-VSCode <bruno.machadots@gmail.com>
2025-09-05 15:46:47 -0300

    Locale correction

--- configs/settings.py
+++ configs/settings.py
@@ -147,10 +147,6 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
-import os
-LOCALE_PATHS = [
-    os.path.join(BASE_DIR, 'locale'),
-]
 
 
 # Static files (CSS, JavaScript, Images)
